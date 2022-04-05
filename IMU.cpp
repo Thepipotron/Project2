0094eddff19b8f291f378e8ae83c39dcdf2e88ec
Thepipotron <payton.fack@okstate.edu>
2022-04-04 19:03:46 -0900

    Finished IMU added MAG for the magnetometer

--- IMU.cpp
+++ IMU.cpp
@@ -10,9 +10,24 @@
 
 #define OUTX_L_G 0x22
 #define OUTX_H_G 0x23
+#define OUTY_L_G 0x24
+#define OUTY_H_G 0x25
+#define OUTZ_L_G 0x26
+#define OUTZ_H_G 0x27
 
 #define OUTX_L_XL 0x28
 #define OUTX_H_XL 0x29
+#define OUTY_L_XL 0x2A
+#define OUTY_H_XL 0x2B
+#define OUTZ_L_XL 0x2C
+#define OUTZ_H_XL 0x2D
+
+#define X 0x0
+#define Y 0x1
+#define Z 0x2
+
+#define G_SCALE 245
+#define X_SCALE 2
 
 //initialized the IMU device depending on the address of the IMU
 int IMU_init(int addr){
@@ -29,31 +44,47 @@ int IMU_init(int addr){
     return device;
 }
 
+//returns the output of the accelerometer with the given direction
+int16_t getAccDir(int dir, int IMU){
+    int16_t accDir = wiringPiI2CReadReg8(IMU, (OUTX_L_XL + (2*dir))) | (wiringPiI2CReadReg8(IMU,OUTX_H_XL+(2*dir)) << 8);
+    return accDir;
+}
 
+//returns the output of the gyro with the given direction
+int16_t getGyroDir(int dir, int IMU){
+    int16_t gyroDir = wiringPiI2CReadReg8(IMU, (OUTX_L_G + (2*dir))) | (wiringPiI2CReadReg8(IMU,OUTX_H_G+(2*dir)) << 8);
+    return gyroDir;
+}
 
+//returns all IMU data in the order AccX, AccY, AccZ, GyroX, GyroY, GyroZ
+double * getIMUCombined(int IMU){
+    static double imuDat[6];
+    
+    //iterate through each of the directions
+    for(int i = 0; i < 3; i++){
+        imuDat[i] = double(getAccDir(i,IMU))/INT16_MAX * X_SCALE;
+        imuDat[i+3] = double(getGyroDir(i,IMU))/INT16_MAX * G_SCALE;
+    }
+
+    return imuDat;
+}
 
 int main(){
     
     int IMU = IMU_init(0x6a);
 
+    std::cout.unsetf(std::ios::floatfield);
+    std::cout.precision(4);
+
     while(1){
         //lets do a quick test and make sure its working
+        double *Output = getIMUCombined(IMU);
 
-        uint8_t ctrl1 = wiringPiI2CReadReg8(IMU,CTRL5_C);
+        for(int i = 0; i < 6; i++){
+            std::cout<<"\t"<<Output[i];
+        }
 
-        //std::cout << "Val :" << unsigned(ctrl1) << std::endl;
-
-
-        //get x accl
-        int16_t acc_x = wiringPiI2CReadReg8(IMU, OUTX_L_XL) | (wiringPiI2CReadReg8(IMU,OUTX_H_XL) << 8);;
-        
-        //get x gyro
-        int16_t gyro_x = wiringPiI2CReadReg8(IMU, OUTX_L_G) | (wiringPiI2CReadReg8(IMU,OUTX_H_G) << 8);
-
-        double acc = (double(acc_x)/ INT16_MAX) * 2;
-        double gyro = (double(gyro_x) / INT16_MAX) * 245;
-
-        std::cout<<"ACCX: " << acc << " GYROX: " << gyro<<std::endl;
+        std::cout<<std::endl;
     }
     return 0;
 }
